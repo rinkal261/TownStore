b08491e621c8636c8de6ed786ba275f40204f6c4
rinkal <rinkalteli@gmail.com>
2023-09-13 00:23:44 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import Product from "./pages/Product";
+import Home from "./pages/Home";
+import ProductList from "./pages/ProductList";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Cart from "./pages/Cart";
+
+const App = () => {
+   return(
+    <>
+    <Home/>
+   <Product/>
+   <ProductList/>
+   <Register/>
+   <Login/>
+   <Cart/>
+   </>
+
+   )
+   
+};
+
+export default App;
